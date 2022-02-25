e80ee28fa850364204edc2c2dcb36fe399cfd371
Knoblauchpilze <prolific-programmer@tuta.com>
2022-02-25 16:10:54 +0100

    feat: Created a script to ease the migration of the base app to another project

---
+++ configureProject.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+## Usage function
+display_usage () {
+  bold=$(tput bold)
+  normal=$(tput sgr0)
+
+  echo "${bold}This script update files to match the provided project name"
+  echo "${bold}Usage:${normal}"
+  echo "./configureProject.sh project_name"
+}
+
+# Make sure that we have at least one argument and
+# that it is valid.
+if [ $# -lt 1 ]; then
+  display_usage
+  exit 1
+fi
+
+PROJECT_NAME=${1}
+
+if [ "${PROJECT_NAME}" == "" ]; then
+  echo "Warning: Empty project name provided"
+  display_usage
+  exit 1
+fi
+
+echo "Renaming project to \"${PROJECT_NAME}\"..."
+
+echo "Updating source files..."
+sed -i "s/pge-app/${PROJECT_NAME}/g" main.cpp
+
+echo "Updating CMakeLists files..."
+sed -i "s/main-app\b/${PROJECT_NAME}/g" CMakeLists.txt
+
+echo "Updating srcipts..."
+sed -i "s/pge-app/${PROJECT_NAME}/g" data/debug.sh
+sed -i "s/pge-app/${PROJECT_NAME}/g" data/profile.sh
+sed -i "s/pge-app/${PROJECT_NAME}/g" data/run.sh
+sed -i "s/pge-app/${PROJECT_NAME}/g" data/valgrind.sh
+
+echo "Cleaning data..."
+make clean
+make cleanSandbox
+
+echo "Rebuilding..."
+make sandbox
+
+echo "Reset git versions..."
+rm -rf .git
+git init
+
+echo "Success !"

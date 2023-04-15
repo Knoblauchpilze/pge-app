a5ba28ab9b4eceecc5f2e2758bd46c8546a9a65e
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-04-15 22:08:26 +0200

    feat: Refactored tests (again) and added Scale test

--- tests/unit/lib/coordinates/CenteredViewportTest.cpp
+++ tests/unit/lib/coordinates/CenteredViewportTest.cpp
@@ -28,60 +28,55 @@ auto generateCenteredTestCaseAbsolute(const std::string &name,
   return TestCaseAbsolute{name, generateCenteredViewportI, coords, expected};
 }
 
-TEST(CenteredViewport_Constructor, Test)
+TEST(Unit_CenteredViewport, Constructor)
 {
-  const auto tlv = generateCenteredViewportI();
+  const auto viewport = generateCenteredViewportI();
 
   const auto topLeft = olc::vi2d(CENTER.x - DIMS.x / 2, CENTER.y + DIMS.y / 2);
-  EXPECT_EQ(tlv->topLeft(), topLeft);
-  EXPECT_EQ(tlv->center(), CENTER);
-  EXPECT_EQ(tlv->dims(), DIMS);
+  EXPECT_EQ(viewport->topLeft(), topLeft);
+  EXPECT_EQ(viewport->center(), CENTER);
+  EXPECT_EQ(viewport->dims(), DIMS);
 }
 
 INSTANTIATE_TEST_CASE_P(
-  CenteredViewport_Bounds,
+  Unit_CenteredViewport,
   RelativeCoordinates,
   Values(generateCenteredTestCaseRelative("top_left", {-12, 5}, {0.0f, 0.0f}),
          generateCenteredTestCaseRelative("top_right", {-8, 5}, {1.0f, 0.0f}),
          generateCenteredTestCaseRelative("bottom_right", {-8, 20}, {1.0f, 1.0f}),
-         generateCenteredTestCaseRelative("bottom_left", {-12, 20}, {0.0f, 1.0f})),
-  generateTestNameRelative);
-
-INSTANTIATE_TEST_CASE_P(CenteredViewport_Inside,
-                        RelativeCoordinates,
-                        Values(generateCenteredTestCaseRelative("inside", {-10, 14}, {0.5f, 0.6f})),
-                        generateTestNameRelative);
-
-INSTANTIATE_TEST_CASE_P(
-  CenteredViewport_Outside,
-  RelativeCoordinates,
-  Values(generateCenteredTestCaseRelative("x_too_small", {-14, 14}, {-0.5f, 0.6f}),
+         generateCenteredTestCaseRelative("bottom_left", {-12, 20}, {0.0f, 1.0f}),
+         generateCenteredTestCaseRelative("inside", {-10, 14}, {0.5f, 0.6f}),
+         generateCenteredTestCaseRelative("x_too_small", {-14, 14}, {-0.5f, 0.6f}),
          generateCenteredTestCaseRelative("x_too_large", {-6, 14}, {1.5f, 0.6f}),
          generateCenteredTestCaseRelative("y_too_small", {-10, -4}, {0.5f, -0.6f}),
          generateCenteredTestCaseRelative("y_too_large", {-10, 26}, {0.5f, 1.4f})),
   generateTestNameRelative);
 
 INSTANTIATE_TEST_CASE_P(
-  CenteredViewport_Bounds,
+  Unit_CenteredViewport,
   AbsoluteCoordinates,
   Values(generateCenteredTestCaseAbsolute("top_left", {0.0f, 0.0f}, {-12, 5}),
          generateCenteredTestCaseAbsolute("top_right", {1.0f, 0.0f}, {-8, 5}),
          generateCenteredTestCaseAbsolute("bottom_right", {1.0f, 1.0f}, {-8, 20}),
-         generateCenteredTestCaseAbsolute("bottom_left", {0.0f, 1.0f}, {-12, 20})),
-  generateTestNameAbsolute);
-
-INSTANTIATE_TEST_CASE_P(CenteredViewport_Inside,
-                        AbsoluteCoordinates,
-                        Values(generateCenteredTestCaseAbsolute("inside", {0.5f, 0.6f}, {-10, 14})),
-                        generateTestNameAbsolute);
-
-INSTANTIATE_TEST_CASE_P(
-  CenteredViewport_Outside,
-  AbsoluteCoordinates,
-  Values(generateCenteredTestCaseAbsolute("x_too_small", {-0.5f, 0.6f}, {-14, 14}),
+         generateCenteredTestCaseAbsolute("bottom_left", {0.0f, 1.0f}, {-12, 20}),
+         generateCenteredTestCaseAbsolute("inside", {0.5f, 0.6f}, {-10, 14}),
+         generateCenteredTestCaseAbsolute("x_too_small", {-0.5f, 0.6f}, {-14, 14}),
          generateCenteredTestCaseAbsolute("x_too_large", {1.5f, 0.6f}, {-6, 14}),
          generateCenteredTestCaseAbsolute("y_too_small", {0.5f, -0.6f}, {-10, -4}),
          generateCenteredTestCaseAbsolute("y_too_large", {0.5f, 1.4f}, {-10, 26})),
   generateTestNameAbsolute);
+
+TEST(Unit_CenteredViewport, Scale)
+{
+  const auto viewport = generateCenteredViewportI();
+
+  const auto factor = 2;
+  viewport->scale(factor, factor);
+
+  const auto topLeft = olc::vi2d(CENTER.x - factor * DIMS.x / 2, CENTER.y + factor * DIMS.y / 2);
+  EXPECT_EQ(viewport->topLeft(), topLeft);
+  EXPECT_EQ(viewport->center(), CENTER);
+  EXPECT_EQ(viewport->dims(), DIMS * factor);
+}
 
 } // namespace pge
--- tests/unit/lib/coordinates/TopLeftViewportTest.cpp
+++ tests/unit/lib/coordinates/TopLeftViewportTest.cpp
@@ -29,66 +29,55 @@ auto generateTopLeftTestCaseAbsolute(const std::string &name,
   return TestCaseAbsolute{name, generateTopLeftViewportI, coords, expected};
 }
 
-TEST(TopLeftViewport_Constructor, Test)
+TEST(Unit_TopLeftViewport, Constructor)
 {
-  const auto tlv = generateTopLeftViewportI();
+  const auto viewport = generateTopLeftViewportI();
 
-  EXPECT_EQ(tlv->topLeft(), TOP_LEFT);
+  EXPECT_EQ(viewport->topLeft(), TOP_LEFT);
   const auto center = TOP_LEFT + DIMS / 2;
-  EXPECT_EQ(tlv->center(), center);
-  EXPECT_EQ(tlv->dims(), DIMS);
+  EXPECT_EQ(viewport->center(), center);
+  EXPECT_EQ(viewport->dims(), DIMS);
 }
 
 INSTANTIATE_TEST_CASE_P(
-  TopLeftViewport_Bounds,
+  Unit_TopLeftViewport,
   RelativeCoordinates,
   Values(generateTopLeftTestCaseRelative("top_left", {-12, 5}, {0.0f, 0.0f}),
          generateTopLeftTestCaseRelative("top_right", {-8, 5}, {1.0f, 0.0f}),
          generateTopLeftTestCaseRelative("bottom_right", {-8, 20}, {1.0f, 1.0f}),
-         generateTopLeftTestCaseRelative("bottom_left", {-12, 20}, {0.0f, 1.0f})),
-  generateTestNameRelative);
-
-INSTANTIATE_TEST_CASE_P(TopLeftViewport_Inside,
-                        RelativeCoordinates,
-                        Values(generateTopLeftTestCaseRelative("inside", {-10, 14}, {0.5f, 0.6f})),
-                        generateTestNameRelative);
-
-INSTANTIATE_TEST_CASE_P(
-  TopLeftViewport_Outside,
-  RelativeCoordinates,
-  Values(generateTopLeftTestCaseRelative("x_too_small", {-14, 14}, {-0.5f, 0.6f}),
+         generateTopLeftTestCaseRelative("bottom_left", {-12, 20}, {0.0f, 1.0f}),
+         generateTopLeftTestCaseRelative("inside", {-10, 14}, {0.5f, 0.6f}),
+         generateTopLeftTestCaseRelative("x_too_small", {-14, 14}, {-0.5f, 0.6f}),
          generateTopLeftTestCaseRelative("x_too_large", {-6, 14}, {1.5f, 0.6f}),
          generateTopLeftTestCaseRelative("y_too_small", {-10, -4}, {0.5f, -0.6f}),
          generateTopLeftTestCaseRelative("y_too_large", {-10, 26}, {0.5f, 1.4f})),
   generateTestNameRelative);
 
 INSTANTIATE_TEST_CASE_P(
-  TopLeftViewport_Bounds,
+  Unit_TopLeftViewport,
   AbsoluteCoordinates,
   Values(generateTopLeftTestCaseAbsolute("top_left", {0.0f, 0.0f}, {-12, 5}),
          generateTopLeftTestCaseAbsolute("top_right", {1.0f, 0.0f}, {-8, 5}),
          generateTopLeftTestCaseAbsolute("bottom_right", {1.0f, 1.0f}, {-8, 20}),
-         generateTopLeftTestCaseAbsolute("bottom_left", {0.0f, 1.0f}, {-12, 20})),
-  generateTestNameAbsolute);
-
-INSTANTIATE_TEST_CASE_P(TopLeftViewport_Inside,
-                        AbsoluteCoordinates,
-                        Values(generateTopLeftTestCaseAbsolute("inside", {0.5f, 0.6f}, {-10, 14})),
-                        generateTestNameAbsolute);
-
-INSTANTIATE_TEST_CASE_P(
-  TopLeftViewport_Outside,
-  AbsoluteCoordinates,
-  Values(generateTopLeftTestCaseAbsolute("x_too_small", {-0.5f, 0.6f}, {-14, 14}),
+         generateTopLeftTestCaseAbsolute("bottom_left", {0.0f, 1.0f}, {-12, 20}),
+         generateTopLeftTestCaseAbsolute("inside", {0.5f, 0.6f}, {-10, 14}),
+         generateTopLeftTestCaseAbsolute("x_too_small", {-0.5f, 0.6f}, {-14, 14}),
          generateTopLeftTestCaseAbsolute("x_too_large", {1.5f, 0.6f}, {-6, 14}),
          generateTopLeftTestCaseAbsolute("y_too_small", {0.5f, -0.6f}, {-10, -4}),
          generateTopLeftTestCaseAbsolute("y_too_large", {0.5f, 1.4f}, {-10, 26})),
   generateTestNameAbsolute);
 
-TEST(TopLeftViewport_MoveTo, Test)
+TEST(Unit_TopLeftViewport, Scale)
 {
-  // virtual void
-  // moveTo(const Vector& position) noexcept = 0;
+  const auto viewport = generateTopLeftViewportI();
+
+  const auto factor = 2;
+  viewport->scale(factor, factor);
+
+  EXPECT_EQ(viewport->topLeft(), TOP_LEFT);
+  const auto center = TOP_LEFT + factor * DIMS / 2;
+  EXPECT_EQ(viewport->center(), center);
+  EXPECT_EQ(viewport->dims(), DIMS * factor);
 }
 
 } // namespace pge

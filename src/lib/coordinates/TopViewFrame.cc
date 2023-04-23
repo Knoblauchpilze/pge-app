3295428d1aa227eb7e3bb8a42984d79fb7c98c1e
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-04-23 21:04:02 +0200

    feat: Refined expression

--- src/lib/coordinates/TopViewFrame.cc
+++ src/lib/coordinates/TopViewFrame.cc
@@ -15,9 +15,16 @@ CenteredViewport TopViewFrame::cellsViewport() const noexcept
 olc::vf2d TopViewFrame::tilesToPixels(float x, float y) const noexcept
 {
   auto rel = m_cellsViewport.relativeCoords(x, y);
-  // cells viewport is centered, pixels viewport is top left based.
+  // Cells viewport is centered, pixels viewport is top left based.
+  // The minimum coordinate for the centered is `-1` and so we need
+  // to offset this. Also as it ranges from `[-1; 1]`, we need to
+  // half the value as the pixels only ranges from `[0; 1]`.
   rel.x = (rel.x + 1.0f) / 2.0f;
-  // cells viewport is with y up, pixels viewport is with y down.
+  // Cells viewport is with y up, pixels viewport is with y down so
+  // the 1 of the cells is the 0 of the pixels and so we need to map
+  // negatively `y`. Similarly as for `x`, cells are ranging from
+  // `[-1; 1]` while pixels go from `[0; 1]` so we need to half the
+  // value.
   rel.y = (1.0f - rel.y) / 2.0f;
   return m_pixelsViewport.absoluteCoords(rel.x, rel.y);
 }

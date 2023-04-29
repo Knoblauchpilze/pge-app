51ba4c81a850fda021e76195ba577b04bf437c0d
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-04-29 09:39:29 +0200

    feat: Links

--- src/lib/coordinates/IsometricViewFrame.cc
+++ src/lib/coordinates/IsometricViewFrame.cc
@@ -1,6 +1,15 @@
 
 #include "IsometricViewFrame.hh"
 
+/// Links:
+/// http://archive.gamedev.net/archive/reference/programming/features/mm4ihm/index.html
+/// https://gamedev.stackexchange.com/questions/142784/how-do-you-transform-a-coordinate-system-from-cartesian-to-isometric
+/// https://gamedev.stackexchange.com/questions/31113/how-can-i-improve-my-isometric-tile-picking-algorithm/31124#31124
+/// https://stackoverflow.com/questions/21842814/mouse-position-to-isometric-tile-including-height
+
+/// Promising:
+/// https://pikuma.com/blog/isometric-projection-in-games
+
 namespace pge {
 
 IsometricViewFrame::IsometricViewFrame(const CenteredViewport &cells, const TopLeftViewport &pixels)

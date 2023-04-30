fb5adf36dc162b63e1980fc4a60b08571e71e5fd
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-04-30 14:42:55 +0200

    fix: Isometric is now using pi/4 rotation

--- src/lib/coordinates/IsometricViewFrame.hxx
+++ src/lib/coordinates/IsometricViewFrame.hxx
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "IsometricViewFrame.hh"
+#include <numbers>
 
 namespace pge {
 
@@ -12,7 +13,9 @@ inline IsometricViewFrame::IsometricViewFrame(const CenteredViewport &tiles,
   // Eigen uses clockwise rotations, so to rotate in the usual trigonometric
   // way we have to invert the angle.
   // https://eigen.tuxfamily.org/dox/classEigen_1_1Rotation2D.html
-  TransformedViewFrame(tiles, Eigen::Rotation2Df(-3.14159f / 4.0f).toRotationMatrix(), pixels)
+  TransformedViewFrame(tiles,
+                       Eigen::Rotation2Df(-std::numbers::pi / 4.0f).toRotationMatrix(),
+                       pixels)
 {}
 
 } // namespace pge

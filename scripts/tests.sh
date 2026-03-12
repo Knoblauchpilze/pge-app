3989e957955de4a7cc34b6567171c8d1f5b29f05
KnoblauchPilze <prolific-programmer@tuta.com>
2026-03-12 14:40:25 +0100

    fix: Test script interpret filters

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 app_name"
+  echo "Usage: $0 app_name [filters]"
+  echo "filters: if provided, will be passed as --gtest_filter to the test binary"
   exit 1
 fi
 
 APP_NAME=$1
 
-./bin/${APP_NAME}
+FILTERS=""
+if [ $# -ge 2 ]; then
+  # https://stackoverflow.com/questions/12076072/how-to-run-specific-test-cases-in-googletest
+  FILTERS="--gtest_filter=$2"
+fi
+
+REPEAT=""
+if [ $# -ge 3 ]; then
+  # https://stackoverflow.com/questions/62466745/whats-the-proper-way-using-gtest-to-repeat-a-multi-threaded-test-multiple-times
+  REPEAT="--gtest_repeat=$3"
+fi
+
+./bin/${APP_NAME} ${FILTERS} ${REPEAT}
